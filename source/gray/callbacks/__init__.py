307f9ee33dc16752e8202ce758085d129a28386b
SeeingTimes <wacto1998@gmail.com>
2026-08-20 00:03:10 +0800

    refactor: move early stopping into callbacks

---
+++ source/gray/callbacks/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable callbacks for training and evaluation workflows."""
+
+from .early_stopping import EarlyStopping
+
+__all__ = ["EarlyStopping"]
--- source/gray/utils/__init__.py
+++ source/gray/utils/__init__.py
@@ -1,6 +1,5 @@
 """Stable convenience imports for single-responsibility utility modules."""
 
-from .early_stopping import EarlyStopping
 from .io import read_json, write_json
 from .logging import get_logger
 from .seed_everything import seed_everything
@@ -8,4 +7,4 @@ from .seed_worker import seed_worker
 from .torch_generator import torch_generator
 from .tta import tta
 
-__all__ = ["EarlyStopping", "get_logger", "read_json", "seed_everything", "seed_worker", "torch_generator", "tta", "write_json"]
+__all__ = ["get_logger", "read_json", "seed_everything", "seed_worker", "torch_generator", "tta", "write_json"]
